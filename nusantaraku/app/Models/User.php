ea2faa101ab4fbe5a5c44f79ab5b25442d7cb6c8
captainPijay <yudharizal10@gmail.com>
2023-11-29 03:19:34 +0700

    mengubah struktur tabel user dan memberikan relasi one to many dari user ke comment

--- nusantaraku/app/Models/User.php
+++ nusantaraku/app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    public function comment()
+    {
+        $this->hasMany(Comment::class, 'user_id');
+    }
 }
