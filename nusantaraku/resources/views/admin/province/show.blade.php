c8ae6b3aa0d661128dc34ed40411212eb5cf566e
captainPijay <yudharizal10@gmail.com>
2023-12-02 03:00:36 +0700

    fix the province show

--- nusantaraku/resources/views/admin/province/show.blade.php
+++ nusantaraku/resources/views/admin/province/show.blade.php
@@ -46,4 +46,84 @@
               @endforeach
             </div>
 </div>
+<div class="container">
+    <h2>Musik</h2>
+    <div class="card-dis">
+        @foreach ($province->musik as $item)
+            <div class="card">
+                <img class="card-img-top" src="{{ Storage::url($item->gambar) }}" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">{{ $item->category_name }}</h5>
+                  <a href="{{ route('musik.show',$item->id) }}" class="btn btn-primary">Detail</a>
+                  <a href="{{ route('musik.edit',$item->id) }}" class="btn btn-warning my-2">Edit</a>
+                  <form action="{{ route('musik.destroy',$item->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger" style="width: 100%" onclick="return confirm('Yakin Di Hapus?')">Delete</button>
+                </form>
+                </div>
+              </div>
+              @endforeach
+            </div>
+</div>
+<div class="container">
+    <h2>Pakaian</h2>
+    <div class="card-dis">
+        @foreach ($province->pakaian as $item)
+            <div class="card">
+                <img class="card-img-top" src="{{ Storage::url($item->gambar) }}" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">{{ $item->category_name }}</h5>
+                  <a href="{{ route('pakaian.show',$item->id) }}" class="btn btn-primary">Detail</a>
+                  <a href="{{ route('pakaian.edit',$item->id) }}" class="btn btn-warning my-2">Edit</a>
+                  <form action="{{ route('pakaian.destroy',$item->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger" style="width: 100%" onclick="return confirm('Yakin Di Hapus?')">Delete</button>
+                </form>
+                </div>
+              </div>
+              @endforeach
+    </div>
+</div>
+<div class="container">
+    <h2>Rumah Adat</h2>
+    <div class="card-dis">
+        @foreach ($province->rumah as $item)
+            <div class="card">
+                <img class="card-img-top" src="{{ Storage::url($item->gambar) }}" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">{{ $item->category_name }}</h5>
+                  <a href="{{ route('rumah.show',$item->id) }}" class="btn btn-primary">Detail</a>
+                  <a href="{{ route('rumah.edit',$item->id) }}" class="btn btn-warning my-2">Edit</a>
+                  <form action="{{ route('rumah.destroy',$item->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger" style="width: 100%" onclick="return confirm('Yakin Di Hapus?')">Delete</button>
+                </form>
+                </div>
+              </div>
+              @endforeach
+    </div>
+</div>
+<div class="container">
+    <h2>Tarian</h2>
+    <div class="card-dis">
+        @foreach ($province->tari as $item)
+            <div class="card">
+                <img class="card-img-top" src="{{ Storage::url($item->gambar) }}" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">{{ $item->category_name }}</h5>
+                  <a href="{{ route('tari.show',$item->id) }}" class="btn btn-primary">Detail</a>
+                  <a href="{{ route('tari.edit',$item->id) }}" class="btn btn-warning my-2">Edit</a>
+                  <form action="{{ route('tari.destroy',$item->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger" style="width: 100%" onclick="return confirm('Yakin Di Hapus?')">Delete</button>
+                </form>
+                </div>
+              </div>
+              @endforeach
+    </div>
+</div>
 @endsection
