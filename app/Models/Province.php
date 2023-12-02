07806fd2271692dbd69f2b82c2d97d7f5723bbfb
captainPijay <yudharizal10@gmail.com>
2023-12-02 21:38:09 +0700

    slicing homepage

--- app/Models/Province.php
+++ app/Models/Province.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Province extends Model
 {
     use HasFactory;
-    protected $guarded = ['id'];
+    protected $guarded = [''];
     // protected $with = ['masakan', 'musik', 'pakaian', 'rumah', 'tari'];
     public function masakan()
     {
