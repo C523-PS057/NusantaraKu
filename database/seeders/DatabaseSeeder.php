6ed1499284622aec35ffbf724d0e88c3ed005850
Agrey Tosira <agreytosira@gmail.com>
2023-12-08 15:22:22 +0700

    feat: update db seeder and set empty seeder for province data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,42 +26,292 @@ class DatabaseSeeder extends Seeder
             'email_verified_at' => now(),
             'password' => bcrypt('1'),
         ]);
-        Budaya::create([
-            'category_name' => 'Masakan',
+        User::create([
+            'name' => 'Agrey Tosira',
+            'email' => 'agreytosira@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('1'),
         ]);
         Budaya::create([
-            'category_name' => 'Musik',
+            'category_name' => 'Masakan Khas',
         ]);
         Budaya::create([
-            'category_name' => 'Pakaian',
+            'category_name' => 'Alat Musik',
+        ]);
+        Budaya::create([
+            'category_name' => 'Pakaian Adat',
         ]);
         Budaya::create([
             'category_name' => 'Rumah Adat',
         ]);
         Budaya::create([
-            'category_name' => 'Tarian',
+            'category_name' => 'Tari Tradisional',
         ]);
         Province::create([
+            'id' => 1,
             'province_name' => 'Aceh',
             'video_link' => 'https://www.youtube.com/embed/Dxy-fm2ep7A?si=gFxc-tUy_9Y5sIoU',
-            'deskripsi' => 'Aceh (Jawi: اچيه دارالسلام) adalah sebuah provinsi di Indonesia yang ibu kotanya berada di Banda Aceh. Aceh merupakan salah satu provinsi di Indonesia yang diberi status sebagai daerah istimewa dan juga diberi kewenangan otonomi khusus. Aceh terletak di ujung utara pulau Sumatra dan merupakan provinsi paling barat di Indonesia. Menurut hasil sensus Badan Pusat Statistik tahun 2021, jumlah penduduk provinsi ini sekitar 5.333.733 jiwa.[13] Letaknya dekat dengan Kepulauan Andaman dan Nikobar di India dan terpisahkan oleh Laut Andaman. Aceh berbatasan dengan Teluk Benggala dan Laut Andaman di sebelah utara, Samudra Hindia di sebelah barat, Selat Malaka di sebelah timur, dan Sumatera Utara di sebelah tenggara dan selatan.
-
-            Aceh dianggap sebagai tempat dimulainya penyebaran Islam di Indonesia dan memainkan peran penting dalam penyebaran Islam di Asia Tenggara. Pada awal tahun 710–1522 M, Kesultanan Samudera Pasai adalah status Wilayah protektorat Kesultanan Utsmaniyah (1569–1903) lalu menjadi Kesultanan Aceh saat itu adalah negara terkaya, terkuat, dan termakmur di kawasan Selat Malakas',
-
-            'sejarah' => 'Aceh telah dihuni manusia sejak zaman Mesolitikum. Hal ini dibuktikan dengan keberadaan situs Bukit Kerang yang diklaim sebagai peninggalan zaman tersebut di kabupaten Aceh Tamiang. Selain itu, pada Situs Desa Pangkalan juga telah dilakukan ekskavasi serta berhasil ditemukan artefak peninggalan dari zaman Mesolitikum berupa kapak Sumatralith, fragmen gigi manusia, tulang badak, dan beberapa peralatan sederhana lainnya.
-
-            Selain di kabupaten Aceh Tamiang, peninggalan kehidupan prasejarah di Aceh juga ditemukan di dataran tinggi Gayo tepatnya di Ceruk Mendale dan Ceruk Ujung Karang yang terdapat disekitar Danau Laut Tawar. Penemuan situs prasejarah ini mengungkapkan bukti adanya hunian manusia prasejarah yang telah berlangsung di sini pada sekitar 7.400 hingga 5.000 tahun yang lalu.'
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
         ]);
         Province::create([
-            'province_name' => 'Kalimantan Selatan',
-            'video_link' => 'https://www.youtube.com/embed/Dxy-fm2ep7A?si=gFxc-tUy_9Y5sIoU',
-            'deskripsi' => 'Aceh (Jawi: اچيه دارالسلام) adalah sebuah provinsi di Indonesia yang ibu kotanya berada di Banda Aceh. Aceh merupakan salah satu provinsi di Indonesia yang diberi status sebagai daerah istimewa dan juga diberi kewenangan otonomi khusus. Aceh terletak di ujung utara pulau Sumatra dan merupakan provinsi paling barat di Indonesia. Menurut hasil sensus Badan Pusat Statistik tahun 2021, jumlah penduduk provinsi ini sekitar 5.333.733 jiwa.[13] Letaknya dekat dengan Kepulauan Andaman dan Nikobar di India dan terpisahkan oleh Laut Andaman. Aceh berbatasan dengan Teluk Benggala dan Laut Andaman di sebelah utara, Samudra Hindia di sebelah barat, Selat Malaka di sebelah timur, dan Sumatera Utara di sebelah tenggara dan selatan.
-
-            Aceh dianggap sebagai tempat dimulainya penyebaran Islam di Indonesia dan memainkan peran penting dalam penyebaran Islam di Asia Tenggara. Pada awal tahun 710–1522 M, Kesultanan Samudera Pasai adalah status Wilayah protektorat Kesultanan Utsmaniyah (1569–1903) lalu menjadi Kesultanan Aceh saat itu adalah negara terkaya, terkuat, dan termakmur di kawasan Selat Malakas',
-
-            'sejarah' => 'Aceh telah dihuni manusia sejak zaman Mesolitikum. Hal ini dibuktikan dengan keberadaan situs Bukit Kerang yang diklaim sebagai peninggalan zaman tersebut di kabupaten Aceh Tamiang. Selain itu, pada Situs Desa Pangkalan juga telah dilakukan ekskavasi serta berhasil ditemukan artefak peninggalan dari zaman Mesolitikum berupa kapak Sumatralith, fragmen gigi manusia, tulang badak, dan beberapa peralatan sederhana lainnya.
-
-            Selain di kabupaten Aceh Tamiang, peninggalan kehidupan prasejarah di Aceh juga ditemukan di dataran tinggi Gayo tepatnya di Ceruk Mendale dan Ceruk Ujung Karang yang terdapat disekitar Danau Laut Tawar. Penemuan situs prasejarah ini mengungkapkan bukti adanya hunian manusia prasejarah yang telah berlangsung di sini pada sekitar 7.400 hingga 5.000 tahun yang lalu.'
+            'id' => 2,
+            'province_name' => 'Bali',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
         ]);
+        Province::create([
+            'id' => 3,
+            'province_name' => 'Bangka Belitung',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]);
+        Province::create([
+            'id' => 4,
+            'province_name' => 'Banten',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]);
+        Province::create([
+            'id' => 5,
+            'province_name' => 'Bengkulu',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]);
+        Province::create([
+            'id' => 6,
+            'province_name' => 'DKI Jakarta',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]);
+        Province::create([
+            'id' => 7,
+            'province_name' => 'Gorontalo',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]);
+        Province::create([
+            'id' => 8,
+            'province_name' => 'Jambi',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 9,
+            'province_name' => 'Jawa Barat',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 10,
+            'province_name' => 'Jawa Tengah',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 11,
+            'province_name' => 'Jawa Timur',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 12,
+            'province_name' => 'Kalimantan Barat',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 13,
+            'province_name' => 'Kalimantan Selatan',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 14,
+            'province_name' => 'Kalimantan Tengah',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 15,
+            'province_name' => 'Kalimantan Timur',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 16,
+            'province_name' => 'Kalimantan Utara',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 17,
+            'province_name' => 'Kepulauan Riau',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 18,
+            'province_name' => 'Lampung',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 19,
+            'province_name' => 'Maluku',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 20,
+            'province_name' => 'Maluku Utara',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 21,
+            'province_name' => 'Nusa Tenggara Timur',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 22,
+            'province_name' => 'Nusa Tenggara Barat',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 23,
+            'province_name' => 'Papua',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 24,
+            'province_name' => 'Papua Barat',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 25,
+            'province_name' => 'Papua Barat Daya',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 26,
+            'province_name' => 'Papua Pegunungan',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 27,
+            'province_name' => 'Papua Selatan',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 28,
+            'province_name' => 'Papua Tengah',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 29,
+            'province_name' => 'Riau',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 30,
+            'province_name' => 'Sulawesi Barat',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 31,
+            'province_name' => 'Sulawesi Selatan',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 32,
+            'province_name' => 'Sulawesi Tengah',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 33,
+            'province_name' => 'Sulawesi Tenggara',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 34,
+            'province_name' => 'Sulawesi Utara',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 35,
+            'province_name' => 'Sulawesi Barat',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 36,
+            'province_name' => 'Sulawesi Selatan',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 37,
+            'province_name' => 'Sumatera Utara',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
+        Province::create([
+            'id' => 38,
+            'province_name' => 'Yogyakarta',
+            'video_link' => '#',
+            'deskripsi' => 'kosong',
+            'sejarah' => 'kosong'
+        ]); 
     }
 }
