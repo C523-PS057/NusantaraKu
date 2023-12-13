64b3384d4d03e4a86a0b79da0f26d4ab95b13baf
Agrey Tosira <agreytosira@gmail.com>
2023-12-13 14:19:34 +0700

    style: fix navbar issue

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -1,13 +1,10 @@
 <header class="navbar navbar-dark sticky-top bg-dark p-0 shadow d-flex align-items-center justify-content-between">
-  <a href="/admin/dashboard">
-    <img src="/img/logo-wide-light.svg" alt="Logo NusantaraKu">
-  </a>
+    <a href="/admin/dashboard">
+        <img src="/img/logo-wide-light.svg" alt="Logo NusantaraKu">
+    </a>
     <p class="navbar-brand ">Selamat Datang, <strong>{{ auth()->user()->name }}</strong></p>
-    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
+        data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="navbar-nav">
-      <div class="nav-item text-nowrap">
-      </div>
-    </div>
 </header>
