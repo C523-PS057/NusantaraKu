e0bf5a69da0a9efd87ece3c8b9cd08f28d527dd4
Agrey Tosira <agreytosira@gmail.com>
2023-12-13 14:02:34 +0700

    file: remove frontend folder in main

--- frontend/js/category.js
+++
@@ -1,6 +0,0 @@
-import province from '../data/province.js';
-
-const select = document.querySelector('#filter-provinsi');
-const selectOptions = province.map((prov) => `<option value="${prov.name.toLowerCase()}">${prov.name}</option>`);
-
-select.innerHTML += selectOptions.join('');
